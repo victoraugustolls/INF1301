c3cbf02c71bb875243aa37a503688d27bcbb7a3e
lucasferraco <lucasferracof@gmail.com>
2016-10-11 00:25:50 -0300

    Fixed some errors in CASA.H and defined casa structure in CASA.C

--- T2/src/CASA.C
+++ T2/src/CASA.C
@@ -1,9 +1,57 @@
-//
-//  CASA.c
-//  
-//
-//  Created by Lucas Ferraço on 05/10/16.
-//
-//
+/***************************************************************************
+ *
+ *  $MCD Módulo de definição: CSA  Casa de um tabuleiro para jogo de xadrez
+ *
+ *  Arquivo gerado:              CASA.h
+ *  Letras identificadoras:      CSA
+ *
+ *  Nome da base de software:    ArcabouÁo para a automaÁ„o de testes de programas redigidos em C
+ *  Arquivo da base de software: D:\AUTOTEST\PROJETOS\LISTA.BSW
+ *
+ *  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+ *  Gestor:  LES/DI/PUC-Rio
+ *  Autores: lff
+ *
+ *  $HA Histórico de evolução:
+ *     Versão  Autor    Data     Observações
+ *     1       lff   10/out/2016 início desenvolvimento
+ *
+ ***************************************************************************/
 
-#include "CASA.h"
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+
+#define CASA_OWN
+#include "CASA.H"
+#undef CASA_OWN
+
+#include "PECA.H"
+#include "LISTA.H"
+
+/***********************************************************************
+ *
+ *  $TC Tipo de dados: CSA Descritor da casa
+ *
+ *
+ ***********************************************************************/
+
+typedef struct CSA_tagCasa {
+    
+    PCA_tppPeca peca ;
+    /* Ponteiro para a peça contida na casa */
+    
+    LIS_tppLista listaAmeacantes ;
+    /* Ponteiro para a lista de peças ameaçantes a peça contida na casa */
+    
+    LIS_tppLista listaAmeacados ;
+    /* Ponteiro para a lista de peças ameaçado pela peça contida na casa */
+    
+} CSA_tpCasa ;
+
+
+/********** Fim do mÛdulo de implementaÁ„o:  CSA  Casa de um tabuleiro para jogo de xadrez
+ **********/
+
