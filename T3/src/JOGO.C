71d3d9abf094ed9d1a9d4ef41f5b94c8f3bdc44b
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2016-11-07 08:14:30 -0200

    jogo.c update

--- T3/src/JOGO.C
+++ T3/src/JOGO.C
@@ -26,8 +26,6 @@ struct JGO_tagJuiz
 
 JGO_tpCondRet JGO_CriarJuiz( JGO_tppJuiz * pJuiz )
 {
-    TAB_tpCondRet condRetTabuleiro;
-
     *pJuiz = malloc(sizeof(JGO_tagJuiz));
 
     if(pJuiz == NULL)
@@ -35,22 +33,57 @@ JGO_tpCondRet JGO_CriarJuiz( JGO_tppJuiz * pJuiz )
         return JGO_CondRetFaltouMemoria;
     }
 
-    condRetTabuleiro = TAB_CriarTabuleiro( pJuiz->tabuleiro ) ;
-
-    if(condRetTabuleiro == TAB_CondRetFaltouMemoria)
-    {
-        free(pJuiz);
-        return JGO_CondRetFaltouMemoria;     
-    }
+    pJuiz->tabuleiro = NULL;
 
     return JGO_CondRetOK;
 }
 
-JGO_tpCondRet JGO_DestruirJuiz( JGO_tppJuiz pJuiz ) ;
+JGO_tpCondRet JGO_DestruirJuiz( JGO_tppJuiz pJuiz )
+{
+    JGO_TerminarJogo(pJuiz);
 
-JGO_tpCondRet JGO_IniciarJogo( JGO_tppJuiz pJuiz ) ;
+    if(pJuiz == NULL)
+    {
+        return JGO_CondRetNaoExiste;
+    }
 
-JGO_tpCondRet JGO_TerminarJogo( JGO_tppJuiz pJuiz ) ;
+    free(pJuiz);
+
+    return JGO_CondRetOK;
+}
+
+JGO_tpCondRet JGO_IniciarJogo( JGO_tppJuiz pJuiz )
+{
+    TAB_tpCondRet condRetTabuleiro;
+
+    if( pJuiz->tabuleiro != NULL)
+    {
+        return JGO_CondRetJogoJaIniciado;
+    }
+
+    condRetTabuleiro = TAB_CriarTabuleiro( &(pJuiz->tabuleiro) ) ;
+
+    if(condRetTabuleiro == TAB_CondRetFaltouMemoria)
+    {
+        pJuiz->tabuleiro = NULL;
+        return JGO_CondRetFaltouMemoria;     
+    }
+    return JGO_CondRetOK;
+}
+
+JGO_tpCondRet JGO_TerminarJogo( JGO_tppJuiz pJuiz )
+{
+    if( pJuiz->tabuleiro == NULL)
+    {
+        return JGO_CondRetJogoNaoIniciado;        
+    }
+
+    TAB_DestruirTabuleiro( pJuiz->pTabuleiro ) ;
+
+    pJuiz->tabuleiro = NULL;
+
+    return JGO_CondRetOK;   
+}
 
 
 JGO_tpCondRet JGO_RealizarJogada( JGO_tppJuiz pJuiz, JGO_tpCorJogador corJogadorAtual,
